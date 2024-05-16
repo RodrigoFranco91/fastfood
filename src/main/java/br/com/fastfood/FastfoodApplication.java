3cc1ebd947575b1fa11b82a11b27c4375050761a
Rodrigo Franco de Lima <rodrigofrancodelima@gmail.com>
2024-05-16 19:43:49 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/fastfood/FastfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodApplication.class, args);
+	}
+
+}
